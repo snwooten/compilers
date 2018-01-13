01168277cf043e97f4f2aa3638a884f48e8d4d87
Sarah Wooten <snwooten@gmail.com>
2018-01-13 14:53:18 -0500

    updated compilers.js

--- calculator.js
+++ calculator.js
@@ -5,13 +5,55 @@ function Calculator (inputString) {
 
 }
 
-Calculator.prototype.peek = function() {
+Calculator.prototype.lexer = function(inputString) {
+  //function that breaks input string into tokens
+  var tokenTypes = [
+    ["NUMBER",    /^\d+/ ],
+    ["ADD",       /^\+/  ],
+    ["SUB",       /^\-/  ],
+    ["MUL",       /^\*/  ],
+    ["DIV",       /^\//  ],
+    ["LPAREN",    /^\(/  ],
+    ["RPAREN",    /^\)/  ]
+  ];
+  var matched = true;
+  var tokens = [];
+    for (let i = 0; i < inputString.length; i++) {
+      for(let j = 0; j < tokenTypes.length; j++) {
+        if(tokenTypes[j][1].test(inputString(i))) {
+          matched = true;
+          tokens.push({name: tokenTypes[j][0], value: inputString[i]});
+        } else {
+          matched = false;
+        }
+      } if(!matched) {
+        throw new Error('Found unparseable token: ' + inputString(i));
+      }
+    }
+    console.log(tokens);
+    return tokens;
+}
 
+
+
+Calculator.prototype.peek = function() {
+  let i = 0;
+  var lexerIterator = function () {
+    return this.lexer[i];
+  };
+ i++;
+
+  return lexerIterator;
 }
 
 Calculator.prototype.get = function() {
-
+  let i = 0;
+  return this.lexer.splice(0,1);
 }
+
+const calc = new Calculator(1+2);
+
+
 
 Calculator.prototype.parseExpression = function() {
 
