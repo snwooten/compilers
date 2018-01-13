c24da63e58b5994aef1c0b9281a7d49003d00a1c
Sarah Wooten <snwooten@gmail.com>
2018-01-13 13:54:02 -0500

    first commit

---
+++ calculator.js
@@ -0,0 +1,42 @@
+// the first part of our parser converts a string into an array (or stream) of tokens that are recognized by our language. These tokens don't have any relation to each other (except being all in the same array).
+
+
+function Calculator (inputString) {
+
+}
+
+Calculator.prototype.peek = function() {
+
+}
+
+Calculator.prototype.get = function() {
+
+}
+
+Calculator.prototype.parseExpression = function() {
+
+}
+
+Calculator.prototype.parseExpression = function() {
+
+}
+
+Calculator.prototype.parseA = function() {
+
+}
+
+Calculator.prototype.parseTerm = function() {
+
+}
+
+Calculator.prototype.parseB = function() {
+
+}
+
+Calculator.prototype.parseFactor = function() {
+
+}
+
+function TreeNode(name, ...children) {
+
+}
